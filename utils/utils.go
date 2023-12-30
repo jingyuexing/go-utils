303485a552145fe5bb0bf391ac81f3a4e52d22fe
jingyuexing <19589872+jingyuexing@users.noreply.github.com>
2023-12-31 06:16:19 +0800

    ✨ feat: add `ToString` and `AccessNested`

--- utils/utils.go
+++ utils/utils.go
@@ -11,21 +11,39 @@ import (
 	"time"
 )
 
+func ToString(val any) string {
+	valstr := ""
+	switch v := val.(type) {
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		valstr = fmt.Sprintf("%d", v)
+	case bool:
+		valstr = fmt.Sprintf("%v", v)
+	case string:
+		valstr = fmt.Sprintf("%s", v)
+	default:
+		valstr = fmt.Sprintf("%v", v)
+	}
+	return valstr
+}
+
 func Template(source string, data map[string]any) string {
+	tempSyntax := "{}"
+	key := ""
 	sourceCopy := &source
-    for k, val := range data {
-        valStr := ""
-        switch v := val.(type) {
-        case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
-            valStr = fmt.Sprintf("%d", v)
-        case bool:
-            valStr = fmt.Sprintf("%v", v)
-        default:
-            valStr = fmt.Sprintf("%s", v)
-        }
-        *sourceCopy = strings.Replace(*sourceCopy, strings.Join([]string{"{", k, "}"}, ""), valStr, 1)
-    }
-    return *sourceCopy
+	for k, val := range data {
+		valStr := ""
+		valStr = ToString(val)
+		switch tempSyntax {
+		case "{}":
+			key = strings.Join([]string{"{", k, "}"}, "")
+		case ":":
+			key = strings.Join([]string{":", k}, "")
+		case "$":
+			key = strings.Join([]string{"$", k}, "")
+		}
+		*sourceCopy = strings.Replace(*sourceCopy, key, valStr, 1)
+	}
+	return *sourceCopy
 }
 
 type Cookie struct {
@@ -55,6 +73,19 @@ func pathParse(target string) []string {
 		}
 	}
 	return obj
+}
+
+func AccessNested(data map[string]interface{}, path string, delimiter string) interface{} {
+	keys := strings.Split(path, delimiter)
+	value := data
+	for _, key := range keys {
+		if val, ok := value[key]; ok {
+			value = val.(map[string]interface{})
+		} else {
+			return nil
+		}
+	}
+	return value
 }
 
 func GetPathValue(raw string, realPath string) map[string]string {
@@ -152,48 +183,46 @@ func Compose[T any](funcs ...func(args ...T) T) func(args ...T) T {
 	return startFunc
 }
 
-
 func TimeDuration(duration string) (time.Time, error) {
-    const (
-        SECOND        uint64 = 1
-        MINUTE_SECOND uint64 = 60 * SECOND
-        HOUR_SECOND   uint64 = 60 * MINUTE_SECOND
-        DAY_SECOND    uint64 = 24 * HOUR_SECOND
-        WEEK_SECOND   uint64 = 7 * DAY_SECOND
-        MONTH_SECOND  uint64 = 30 * DAY_SECOND
-        YEAR_SECOND   uint64 = 12 * MONTH_SECOND
-    )
-    uints := map[string]uint64{
-        "y": YEAR_SECOND,
-        "m": MONTH_SECOND,
-        "w": WEEK_SECOND,
-        "d": DAY_SECOND,
-        "h": HOUR_SECOND,
-        "s": SECOND,
-    }
-    splitDuration := strings.Split(duration, " ")
-    durationSecond := uint64(0)
-    for _, val := range splitDuration {
-        unit := val[len(val)-1:]
-        num, _ := strconv.Atoi(val[:len(val)-1])
-        if unitSeconds, ok := uints[unit]; ok {
-            durationSecond += uint64(num) * unitSeconds
-        } else {
-            return time.Time{}, fmt.Errorf("haven't this unit")
-        }
-    }
-    return time.Now().Add(time.Duration(durationSecond) * time.Second).UTC(), nil
+	const (
+		SECOND        uint64 = 1
+		MINUTE_SECOND uint64 = 60 * SECOND
+		HOUR_SECOND   uint64 = 60 * MINUTE_SECOND
+		DAY_SECOND    uint64 = 24 * HOUR_SECOND
+		WEEK_SECOND   uint64 = 7 * DAY_SECOND
+		MONTH_SECOND  uint64 = 30 * DAY_SECOND
+		YEAR_SECOND   uint64 = 12 * MONTH_SECOND
+	)
+	uints := map[string]uint64{
+		"y": YEAR_SECOND,
+		"m": MONTH_SECOND,
+		"w": WEEK_SECOND,
+		"d": DAY_SECOND,
+		"h": HOUR_SECOND,
+		"s": SECOND,
+	}
+	splitDuration := strings.Split(duration, " ")
+	durationSecond := uint64(0)
+	for _, val := range splitDuration {
+		unit := val[len(val)-1:]
+		num, _ := strconv.Atoi(val[:len(val)-1])
+		if unitSeconds, ok := uints[unit]; ok {
+			durationSecond += uint64(num) * unitSeconds
+		} else {
+			return time.Time{}, fmt.Errorf("haven't this unit")
+		}
+	}
+	return time.Now().Add(time.Duration(durationSecond) * time.Second).UTC(), nil
 }
 
 func Chunk(slice []int, size int) [][]int {
-    var chunks [][]int
-    for i := 0; i < len(slice); i += size {
-        end := i + size
-        if end > len(slice) {
-            end = len(slice)
-        }
-        chunks = append(chunks, slice[i:end])
-    }
-    return chunks
+	var chunks [][]int
+	for i := 0; i < len(slice); i += size {
+		end := i + size
+		if end > len(slice) {
+			end = len(slice)
+		}
+		chunks = append(chunks, slice[i:end])
+	}
+	return chunks
 }
-
